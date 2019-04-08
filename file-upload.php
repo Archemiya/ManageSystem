6e4b0f3fed14caa9d185cef383620c31fd49b51a
archemiya <1257546443@qq.com>
2019-04-08 09:08:16 +0800

    撤销更改

--- file-upload.php
+++ file-upload.php
@@ -1,13 +1,28 @@
 <?php
 
-$uploaddir = './uploaded_file/';
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-
-
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "<script>alert('文件上传成功！');history.go(-1)</script>";
-} else {
-    echo "<script>alert('文件上传失败！请重新上传')</script>";
+if ((($_FILES["file"]["type"] == "application/msword")
+|| ($_FILES["file"]["type"] == "application/pdf")
+|| ($_FILES["file"]["type"] == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"))
+&& ($_FILES["file"]["size"] < 10000000))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Error: " . $_FILES["file"]["error"] . "<br />";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+    echo "Type: " . $_FILES["file"]["type"] . "<br />";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+    echo "Stored in: " . $_FILES["file"]["tmp_name"];
+    }
+  }
+else if($_FILES["file"]["size"] >= 10000000)
+  {
+  echo "<script>alert('文件过大超过10M！请重新上传');history.go(-1)</script>";
+  }
+else if(($_FILES["file"]["type"] != "application/msword")
+||($_FILES["file"]["type"] != "application/pdf")
+|| ($_FILES["file"]["type"] != "application/vnd.openxmlformats-officedocument.wordprocessingml.document")){
+    echo "<script>alert('只允许上传doc/docx/pdf格式文件！请重新上传');history.go(-1)</script>";
 }
-
-?>
