889b3787b6fb795bdcd5603ab9b25d5f2adfc94c
Desktop-archemiya <archemiya@debian.Archemiya>
2019-05-09 21:36:04 +0800

    修改v1.1

--- secretary_func/sec_stu_func_control.php
+++ secretary_func/sec_stu_func_control.php
@@ -314,7 +314,7 @@ $today = date('Y-m-d');
                     <td class="col-xs-2 th-title-center">
                         <?php
                         if (!$num_ishave) {
-                            echo "<button class='btn btn-warning' disabled>不可操作</button>";                            
+                            echo "<button class='btn btn-warning' disabled>不可操作</button>";
                         } elseif ($num_ishave) {
                             if ($num_delay != 0 && $row_control['reply_delay'] == 0) {
                                 echo "<button class='btn btn-warning' disabled>不可操作</button>";
@@ -411,6 +411,11 @@ $today = date('Y-m-d');
                     <td class="col-xs-5 th-title-center">二次答辩
                     </td>
                     <?php
+                    //查看当前时候已有学生
+                    $sql_ishave = "SELECT * FROM `reply_schedule` where `permission` = 'student'";
+                    $result_ishave = mysqli_query($link, $sql_ishave);
+                    $num_ishave = mysqli_num_rows($result_ishave);
+
                     //判断已完成一辩评分的学生数量
                     $sql_final_grade_num = "SELECT * FROM `student_grade` 
                     where `reply_grade_final_flag` = 1 ";
@@ -422,36 +427,43 @@ $today = date('Y-m-d');
                     AND `first_paper_flag` = 1 AND `reply_delay`=0 ";
                     $result_first = mysqli_query($link, $sql_first);
                     $num_first = mysqli_num_rows($result_first);
-
-                    if ($num_first > $num_final_grade_num) {
-                        echo "<td class=\"col-xs-5 th-title-center alert alert-warning\">";
+                    if (!$num_ishave) {
+                        echo "<td class=\"col-xs-5 th-title-center alert alert-warning\" >";
                         echo "当前一辩评分工作尚未完成，请等待导师评分完成";
-                    } elseif ((!$row_control['second_reply'])
-                        && $num_first == $num_final_grade_num
-                    ) {
-                        echo "<td class=\"col-xs-5 th-title-center alert alert-info\">";
-                        echo "请根据学校要求及时打开二次答辩流程";
-                    } else {
-                        echo "<td class=\"col-xs-5 th-title-center alert alert-info\">";
-                        echo "已开启学生二次答辩流程";
+                    } elseif ($num_ishave) {
+                        if ($num_first > $num_final_grade_num) {
+                            echo "<td class=\"col-xs-5 th-title-center alert alert-warning\">";
+                            echo "当前一辩评分工作尚未完成，请等待导师评分完成";
+                        } elseif ((!$row_control['second_reply'])
+                            && $num_first == $num_final_grade_num
+                        ) {
+                            echo "<td class=\"col-xs-5 th-title-center alert alert-info\">";
+                            echo "请根据学校要求及时打开二次答辩流程";
+                        } else {
+                            echo "<td class=\"col-xs-5 th-title-center alert alert-info\">";
+                            echo "已开启学生二次答辩流程";
+                        }
                     }
                     ?>
                     </td>
 
                     <td class="col-xs-2 th-title-center">
                         <?php
-                        if ($num_first > $num_final_grade_num) {
+                        if (!$num_ishave) {
                             echo "<button class='btn btn-warning' disabled>不可操作</button>";
-                        } elseif ((!$row_control['second_reply'])
-                            && $num_first == $num_final_grade_num
-                        ) {
-                            echo "<a href='sec_chang_stu_control_value.php?func=second_reply' 
+                        } elseif ($num_ishave) {
+                            if ($num_first > $num_final_grade_num) {
+                                echo "<button class='btn btn-warning' disabled>不可操作</button>";
+                            } elseif ((!$row_control['second_reply'])
+                                && $num_first == $num_final_grade_num
+                            ) {
+                                echo "<a href='sec_chang_stu_control_value.php?func=second_reply' 
                                 class='btn btn-primary' role='button'
                                 onclick=\"Javascript:return confirm('确定开启么？此操作不可逆转')\">开启二次答辩</a>";
-                        } else {
-                            echo "<a class='btn btn-primary' role='button' disabled>已开启</a>";
+                            } else {
+                                echo "<a class='btn btn-primary' role='button' disabled>已开启</a>";
+                            }
                         }
-
                         ?>
                     </td>
                 </tr>
