669ad7a2008d351b22f3fadad78dda60b4c2b4c9
Desktop-archemiya <archemiya@debian.Archemiya>
2019-05-10 10:36:11 +0800

    修改v1.9

--- tutor_func/t_reply_grade.php
+++ tutor_func/t_reply_grade.php
@@ -80,7 +80,7 @@ archemiya;
         echo   $row_student['id'] . $row_student['name'];
         echo "</td>";
 
-        if (($row_grade['reply_grade'])==NULL) {
+        if (!isset($row_grade['reply_grade'])) {
             if (isset($row_record['reply_record_annex_name'])) {
                 echo "<td>";
                 echo "<a class='btn btn-primary' role='button'
@@ -106,7 +106,7 @@ archemiya;
             </button>";
                 echo "</td>";
             }
-        } elseif (($row_fgrade['reply_grade']) != NULL) {
+        } elseif (($row_fgrade['reply_grade']) != 0) {
             echo "<td class='th-title-center alert alert-info' role='alert'>";
             echo "最终成绩：";
             echo $row_fgrade['reply_grade'];
